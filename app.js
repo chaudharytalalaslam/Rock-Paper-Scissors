59245eee35f67bc31ba43d44966ede1ebd3011fe
Muhammad Talal Aslam <146806220+chaudharytalalaslam@users.noreply.github.com>
2025-06-26 15:20:27 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,91 +1,84 @@
-
 let userScore = 0;
 let compScore = 0;
 let drawScore = 0;
 
-const choices = document.querySelectorAll(".choice");
-const msg = document.querySelector("#msg");
-const uS = document.querySelector("#user-score")
-const cS = document.querySelector("#comp-score")
-const DS = document.querySelector("#draw-score")
+let choices = document.querySelectorAll(".choice")
+let msg = document.querySelector("#msg");
+let uS=document.querySelector("#user-score")
+let cS=document.querySelector("#comp-score")
+let dS=document.querySelector("#draw-score")
+let userChoiceImg=document.querySelector("#user-c")
+let compChoiceImg=document.querySelector("#comp-c")
 
 
-const genCompChoice = () => {
-    //rock paper scissors       // math.random() genrate random values from 0 to 1 
-    const options = ["rock", "paper", "scissors"];
-    const randIdx = Math.floor(Math.random() * 3)
-    return options[randIdx];
+const gencompChoice = () => {
+    const array = ["rock", "paper", "scissors"];
+    const randomIdx = Math.floor(Math.random() * 3)
+    console.log(randomIdx)
+
+    return array[randomIdx];
 }
 
-
-// check the draw condition
-const draw = () => {
-    msg.innerText = "Game Was Draw.Play again"
-    drawScore++
-    msg.style.backgroundColor = "#081b31"
-    DS.innerText=drawScore
-
-
-}
-
-// show the winner user or computer
 const showWinner = (userWin, userChoice, compChoice) => {
     if (userWin) {
-        msg.innerText = `You win  ${userChoice} beats ${compChoice}`;
-        msg.style.backgroundColor = "green"
-        userScore++;
-        uS.innerText = userScore;
+        msg.innerText = `You win ! ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+        userScore++
+        uS.innerText=userScore;
     }
     else {
-        msg.innerText = `You lose  ${compChoice} beats  ${userChoice}`;
+        msg.innerText = `You lose ! ${compChoice} beats ${userChoice}`;
         msg.style.backgroundColor = "red"
         compScore++
-        cS.innerText = compScore;
+        cS.innerText=compScore;
+
 
     }
 }
 
-// check the conditions who is winning or draw
+const draw = () => {
+    msg.innerText = "Game is draw Play Again";
+    drawScore++;
+    msg.style.backgroundColor = "#081b31";
+    dS.innerText=drawScore;
+
+}
 
 const playGame = (userChoice) => {
-    let compChoice = genCompChoice()
-    console.log("User Choice =", userChoice)
-    console.log("Comp Choice =", compChoice)
-
-
-    //draw Game
-
+    let compChoice = gencompChoice()
+    let userWin = true;
+    updateChoiceImages(userChoice,compChoice)
     if (userChoice === compChoice) {
         draw()
     }
 
     else {
-        let userWin = true;
-
+        
         if (userChoice === "rock") {
-            // compChoice will be paper or scissors
             userWin = compChoice === "paper" ? false : true
-        }
 
+        }
         else if (userChoice === "paper") {
-            // compChoice will be rock or scissors
             userWin = compChoice === "scissors" ? false : true
         }
+
         else {
-            // compChoice will be rock or paper
             userWin = compChoice === "rock" ? false : true
         }
-
-        showWinner(userWin, userChoice, compChoice);
     }
+
+    showWinner(userWin, userChoice, compChoice)
 }
 
-// click event to get user choice
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         let userChoice = choice.getAttribute("id")
-        // console.log("choice was clicked ", userChoice)
         playGame(userChoice);
     })
 })
+
+const updateChoiceImages=(userChoice,compChoice)=>{
+    userChoiceImg.src=`./images/${userChoice}.png `;
+    compChoiceImg.src=`./images/${compChoice}.png`;
+}
